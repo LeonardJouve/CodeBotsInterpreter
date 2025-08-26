89d06aca4f2d7d0dfd6c040bf66a87eabdab3463
Léonard Jouve <leonard.jouve@gmail.com>
2025-08-26 18:53:14 +0200

    use parser in repl

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
-export default class Parser {
-    constructor() {
-    }
+import Lexer from "./lexer";
+import Parser from "./parser";
 
-    test(): string {
-        return "test";
+export default class Interpreter {
+    constructor() {}
+
+    repl() {
+        const PROMPT = ">> ";
+
+        process.stdout.write(PROMPT);
+
+        process.stdin.setEncoding("utf-8");
+
+        process.stdin.on("data", (line: string) => {
+            const lexer = new Lexer(line);
+            const parser = new Parser(lexer);
+            const program = parser.parseProgram();
+
+            if (parser.errors.length) {
+                process.stderr.write("Parser error:\n");
+
+                parser.errors.forEach((error) => {
+                    process.stderr.write(`\t${error}\n`);
+                });
+
+                process.stdout.write(PROMPT);
+
+                return;
+            }
+
+            process.stdout.write(`${program.toString()}\n`);
+
+            process.stdout.write(PROMPT);
+        });
     }
 }
