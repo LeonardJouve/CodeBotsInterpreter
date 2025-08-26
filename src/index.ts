87469f809e7b71d86c41b18eb55997ef7f149053
Léonard Jouve <leonard.jouve@gmail.com>
2025-08-26 08:24:55 +0200

    basic REPL

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import Lexer from "./lexer";
+
 export default class Parser {
     constructor() {
     }
@@ -6,3 +8,28 @@ export default class Parser {
         return "test";
     }
 }
+
+const PROMPT = ">> ";
+
+const repl = () => {
+    process.stdout.write(PROMPT);
+
+    process.stdin.setEncoding("utf-8");
+
+    process.stdin.on("data", (chunk: string) => {
+        const line = chunk.trim();
+        if (line.length === 0) {
+            process.exit(0);
+        }
+
+        const lexer = new Lexer(line);
+
+        for (let token = lexer.nextToken(); token.type !== "EOF"; token = lexer.nextToken()) {
+            process.stdout.write(JSON.stringify(token) + "\n");
+        }
+
+        process.stdout.write(PROMPT);
+    });
+};
+
+repl();
